a1e27d312d56fedabc5684fdaec4f9d97d07620e
Azriel <Arilsaputra854@gmail.com>
2023-12-06 00:58:17 +0700

    fix splash screen

--- lib/splash_screen.dart
+++ lib/splash_screen.dart
@@ -1,16 +1,39 @@
 import "package:flutter/material.dart";
-
+import "package:rol_music/home_page.dart";
 
 class splashScreen extends StatelessWidget {
   const splashScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+
+    goToHomeScreen(context);
+
     return Container(
       padding: EdgeInsets.all(10),
-      child: Column(mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
-        Center(child: Image.asset("assets/img/Logo.png")), Text("Created by \n Aril Saputra", style: TextStyle(fontSize: 15,decoration: TextDecoration.none,color: Colors.black),)],),
+          SizedBox(height: 80,),
+          Image.asset("assets/img/Logo.png"),
+          Text(
+            "Created by \n Aril Saputra",
+            style: TextStyle(
+                fontSize: 15,
+                decoration: TextDecoration.none,
+                color: Colors.black),
+          )
+        ],
+      ),
     );
+  }
+
+
+  goToHomeScreen(BuildContext context) async {
+    await Future.delayed(Duration(seconds: 3)).then((value) => {
+       Navigator.pushReplacement(context, MaterialPageRoute(builder: (context){
+        return HomePage();
+       }))
+    });
   }
 }
