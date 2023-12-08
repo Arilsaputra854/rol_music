8696f80921e3fb7dbe7df811930e27db3432137d
Azriel <Arilsaputra854@gmail.com>
2023-12-08 22:49:49 +0700

    Mencoba memperbaiki player

--- lib/player_page.dart
+++ lib/player_page.dart
@@ -5,30 +5,25 @@ import "package:rol_music/model/music.dart";
 class playerPage extends StatefulWidget {
   final List<Music> data;
   final int index;
-  const playerPage(@required  this.data,@required this.index, {super.key});
+  const playerPage(@required this.data, @required this.index, {super.key});
 
   @override
   State<playerPage> createState() => _playerPageState(this.data, this.index);
 }
 
-class _playerPageState extends State<playerPage>
-    with TickerProviderStateMixin {
-    List<Music> data;
-    int index;
+class _playerPageState extends State<playerPage> with TickerProviderStateMixin {
+  List<Music> data;
+  int index;
 
-    _playerPageState(List<Music> this.data, this.index);
-    
+  _playerPageState(List<Music> this.data, this.index);
+
   @override
   Widget build(BuildContext context) {
-
-    final player = AudioPlayer();
-
     Music nowPlaying = data[index];
 
     final AnimationController _controller =
         AnimationController(vsync: this, duration: Duration(seconds: 5))
           ..repeat();
- 
 
     return Scaffold(
         appBar: AppBar(
@@ -57,7 +52,11 @@ class _playerPageState extends State<playerPage>
             ),
             Text(
               nowPlaying.musicName,
-              style: TextStyle(fontSize: 20, fontFamily: "Futura",),textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 20,
+                fontFamily: "Futura",
+              ),
+              textAlign: TextAlign.center,
             ),
             Container(
                 padding: EdgeInsets.only(left: 50, right: 50),
@@ -82,9 +81,9 @@ class _playerPageState extends State<playerPage>
                         iconSize: 30,
                         onPressed: () {
                           setState(() {
-                            index = index-1;
-                            if(index < 0){
-                              index = data.length -1;
+                            index = index - 1;
+                            if (index < 0) {
+                              index = data.length - 1;
                             }
                             nowPlaying = data[index];
                             print("now playing: $nowPlaying");
@@ -101,9 +100,11 @@ class _playerPageState extends State<playerPage>
                     child: IconButton(
                         iconSize: 50,
                         onPressed: () {
+                          final player = AudioPlayer();
                           var src = AssetSource(nowPlaying.musicUrl);
-                          player.play(src);                   
-
+                          print(nowPlaying.musicUrl);
+                          setState(() {});
+                          player.setSource(src);
                         },
                         icon: Icon(
                           Icons.play_arrow,
@@ -117,8 +118,8 @@ class _playerPageState extends State<playerPage>
                         iconSize: 30,
                         onPressed: () {
                           setState(() {
-                            index = index+1;
-                            if(index > data.length -1){
+                            index = index + 1;
+                            if (index > data.length - 1) {
                               index = 0;
                             }
                             nowPlaying = data[index];
